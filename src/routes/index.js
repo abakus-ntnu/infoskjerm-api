dfde3551d8e7fa5e4ff37746b8058db6008eacc3
AsmundH <aasmund.haugse@gmail.com>
2019-01-09 16:58:10 +0100

    Add /version routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import Router from 'koa-router';
 import busRouter from './bus';
 import eventsRouter from './events';
+import versionRouter from './version';
 
 
 const router = new Router();
 router.use('/bus', busRouter.routes(), busRouter.allowedMethods());
 router.use('/events', eventsRouter.routes(), eventsRouter.allowedMethods());
+router.use('/version', versionRouter.routes(), versionRouter.allowedMethods());
 
 
 export default router;
